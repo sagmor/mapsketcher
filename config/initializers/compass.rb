0cdd79c747e2dbfbcf34d4b9eef338d61e274730
Sebastián Gamboa <me@sagmor.com>
2010-09-20 10:20:06 -0400

    Checking if this works.

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,3 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
-
-Sass::Plugin.options[:template_location] = { 'app/stylesheets' => 'public/stylesheets' }
