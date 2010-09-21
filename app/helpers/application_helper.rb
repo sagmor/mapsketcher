0426ee3639a5ee25cdae5944271506fe176006dd
Sebastián Gamboa <me@sagmor.com>
2010-09-21 17:47:15 -0400

    Adding Google Analytics code insertion.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def analytics_code(id = nil)
+    id ||= ENV['GOOGLE_ANALYTICS_ACCOUNT']
+
+    return '' if id.nil?
+    return <<EOC
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '#{id}']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>      
+EOC
+  end
 end
