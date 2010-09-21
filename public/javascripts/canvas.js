02bd9c5dc61d128bacff80ffd4553aa6b5ed83e1
Sebastián Gamboa <me@sagmor.com>
2010-09-21 19:23:43 -0400

    Adding support for touch devises.

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -7,9 +7,11 @@ var Canvas = (function(){
     this.dom[0].canvas = this;
     
     this.dom.mousedown(this.onDrawStart);
+    this.dom.bind('touchstart',this.onDrawStart);
     this.dom.mousemove(this.onDraw);
+    this.dom.bind('touchmove',this.onDraw);
     this.dom.mouseup(this.onDrawEnd);
-    this.dom.dblclick(this.disable);
+    this.dom.bind('touchend',this.onDrawEnd);
   }
   
   Canvas.prototype.enable = function() {
@@ -33,9 +35,18 @@ var Canvas = (function(){
   }
   
   Canvas.prototype.onDraw = function(event) {
+    event.preventDefault();
     if (!this.canvas.drawing) return;
     
-    var location = this.canvas.overlay.getProjection().fromContainerPixelToLatLng(new google.maps.Point(event.clientX, event.clientY));
+    event = event.originalEvent || event;
+
+    // This is not a touch devise?
+    if (!event.touches) event.touches = [event];
+    if (event.touches.length > 1) return;
+
+    var touch = event.touches[0];
+    
+    var location = this.canvas.overlay.getProjection().fromContainerPixelToLatLng(new google.maps.Point(touch.pageX, touch.pageY));
     this.canvas.sketch.append(location.lat(),location.lng());
     
     if (this.canvas.polyline) this.canvas.polyline.setMap(null);
@@ -44,12 +55,20 @@ var Canvas = (function(){
   }
   
   Canvas.prototype.onDrawEnd = function(event) {
-    this.canvas.onDraw.call(this, event);
     this.canvas.drawing = false;
     
     var polyline = this.canvas.polyline;
     this.canvas.polyline = null;
     this.canvas.sketch.save(this.canvas.map.objects);
+
+    // Checking double Click/Tap
+    var now = new Date().getTime();
+    var lastTouch = this.canvas.lastTouch || now + 1; // the first time this will make delta a negative number
+
+    var delta = now - lastTouch;
+
+    if(delta <500 && delta>0) this.canvas.disable.call(this, event);
+    this.canvas.lastTouch = now;
   }
   
  
