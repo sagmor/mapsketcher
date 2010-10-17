4dde13a01891cfa6d3dbad8aed58ba49d0dcec83
Sebastián Gamboa <me@sagmor.com>
2010-10-17 18:25:34 -0300

    Adding Modal dialog for Collaborative Room Selection. References #19

--- public/js/application.js
+++ public/js/application.js
@@ -4,5 +4,21 @@ jQuery(function() {
     config.hostname = window.location.hostname
     mapSketcherClient = new MapSketcherClient(config);
     mapSketcherClient.launch();
-  })
+  });
+
+  $("a[rel]").overlay({
+	  mask: {
+		  color: '#ebecff'
+		, loadSpeed: 200
+		, opacity: 0.9
+    }
+  });
+
+  $('#collaborativeRoomSelection form').submit(function(e) {
+    $('.close').click();
+
+    // TODO: Load Room
+
+    return e.preventDefault();
+  });
 });
